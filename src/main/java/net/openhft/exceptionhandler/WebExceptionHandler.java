12dc430cde80915126bcef34d3fd64a29e7da415
Frank Dietrich <frank.dietrich@gmx.li>
2016-04-08 12:53:15 +0200

    include the class name in uri, also in cases where exception.getMessage() is not null

--- src/main/java/net/openhft/exceptionhandler/WebExceptionHandler.java
+++ src/main/java/net/openhft/exceptionhandler/WebExceptionHandler.java
@@ -42,7 +42,7 @@ public class WebExceptionHandler implements ExceptionHandler {
                         version = parts[1];
                     }
 
-                    uri += "+" + version + "+" + t;
+                    uri += "+" + version + "+" + t.getClass().getName();
                     if (message != null)
                         uri += "+" + URIEncoder.encodeURI(message);
                 }
